f2cc220919ee90440e3170b77f821c7761293581
Tor.Puntawus <tor.puntawus@hotmail.com>
2015-08-21 17:11:06 +0700

    -- Lab 1-2 Three functions: --

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -10,34 +10,54 @@ namespace Lab1_2
     {
         static void Main(string[] args)
         {
+            BubbleSortFunction s = new BubbleSortFunction();
+            int[] a = s.inputFunc();
+            s.processFunc(a);
+            s.outputFunc(a);
+            Console.ReadKey();
+        }
+
+         public int[] inputFunc()
+         {
+             int num;
+
+             Console.WriteLine("Input how many integers: ");
+             num = Convert.ToInt32(Console.ReadLine());
+
+             int[] a = new int[num];
+             for (int i = 0; i < num; i++)
+             {
+                 a[i] = Convert.ToInt32(Console.ReadLine());
+             }
+             return a;
+        }
+        
+        public void processFunc(int[] a)
+        {
             bool flag = true;
-
-            // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
             while (flag)
             {
                 flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                for (int i = 0; i != a.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    if (a[i] > a[i + 1])
                     {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
                         flag = true;
                     }
                 }
             }
+        }
 
-            // output
-            for (int i = 0; i != input.Length; i++)
+        public void outputFunc(int[] a)
+        {
+            for (int i = 0; i != a.Length; i++)
             {
-                Console.Write(input[i]);
+                Console.Write(a[i]);
                 Console.Write(" ");
             }
-            Console.ReadKey();
         }
     }
 }
